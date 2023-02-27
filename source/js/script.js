90734b98b6c1c7fcf85c5c27ec99ba4ef36993c5
Dariamoys <dash2004@inbox.ru>
2023-02-27 16:06:09 +0300

    Оживляет слайдер и добавляет карту

---
+++ source/js/script.js
@@ -0,0 +1,58 @@
+let navMain = document.querySelector('.main-navigation');
+let navToggle = document.querySelector('.main-navigation__toggle');
+
+navMain.classList.remove('main-navigation--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-navigation--closed')) {
+    navMain.classList.remove('main-navigation--closed');
+    navMain.classList.add('main-navigation--opened');
+  } else {
+    navMain.classList.add('main-navigation--closed');
+    navMain.classList.remove('main-navigation--opened');
+  }
+});
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const position = [59.968266, 30.3174089];
+const map = L.map('map-wrapper').setView(position, 17);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/map-pin.svg',
+  iconSize: [39, 50],
+  iconAnchor: [18, 50],
+});
+
+const mainPin = L.marker(
+  {
+    lat: position[0],
+    lng: position[1],
+  },
+  {
+    draggable: false,
+    icon: mainPinIcon,
+  },
+);
+mainPin.addTo(map);
